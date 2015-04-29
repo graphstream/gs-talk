d648ab97ab0d5cdad4cf25f0ef7e68d4c2b77c90
Thibaut Démare <fdhp_76@hotmail.com>
2015-04-29 03:04:09 +0200

    Add slides about interaction with GAMA

--- Demos/EclipseProject/src/org/graphstream/demo/gamainteraction/ReceiverDemo.java
+++ Demos/EclipseProject/src/org/graphstream/demo/gamainteraction/ReceiverDemo.java
@@ -3,12 +3,16 @@ package org.graphstream.demo.gamainteraction;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.stream.netstream.NetStreamReceiver;
+import org.graphstream.ui.swingViewer.Viewer;
 
 public class ReceiverDemo {
 
 	public static void main(String[] args) throws InterruptedException, UnknownHostException, IOException {
-		NetStreamReceiver receiver1 = new NetStreamReceiver(2001);
-		new NetStreamViewer(receiver1, false);
+		NetStreamReceiver receiver = new NetStreamReceiver("localhost", 2001);
+		Viewer v = new Viewer(receiver.getDefaultStream());
+		v.addDefaultView(true);
 	}
 }
